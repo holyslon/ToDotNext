cfc4224d35d4cac3909ab365a159370954508b29
Anton Onikiychuk <anton@onikiychuk.com>
2014-04-03 14:07:09 +0400

    add test that we dont remove tag from middle of the string (just remove hash sign

--- ToDo.Tests/MainWindowViewModelTests.cs
+++ ToDo.Tests/MainWindowViewModelTests.cs
@@ -39,5 +39,27 @@ namespace ToDo.Tests
             Assert.Equal(tags[1], mainWindowViewModel.Assigments[0].Tags[1].Text);
             Assert.Equal(tags[2], mainWindowViewModel.Assigments[0].Tags[2].Text);
         }
+
+        [Fact]
+        public void DoesNotRemoveTagFromMiddleOffString()
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+
+            const string newAssigmentText = "some new assigment text";
+            var tags = new[] { "work", "home", "csharp" };
+
+            var fullAssigmentString = "#dotnext " + newAssigmentText + " " + string.Join(" ", tags.Select(s => "#" + s));
+
+            mainWindowViewModel.AssigmentText = fullAssigmentString;
+
+            mainWindowViewModel.AddAssigment.Execute(null);
+
+            Assert.Equal("dotnext " + newAssigmentText, mainWindowViewModel.Assigments[0].Text);
+            Assert.Equal("dotnext", mainWindowViewModel.Assigments[0].Tags[0].Text);
+            Assert.Equal(tags[0], mainWindowViewModel.Assigments[0].Tags[1].Text);
+            Assert.Equal(tags[1], mainWindowViewModel.Assigments[0].Tags[2].Text);
+            Assert.Equal(tags[2], mainWindowViewModel.Assigments[0].Tags[3].Text);
+            
+        }
     }
 }
